e14468d2e345c8bd7ddb1760469216b4df17ee97
Soonoh Jung <soonohjung@mytrum.com>
2015-11-27 17:26:44 +0900

    Update jdk version to 8

--- bin/java_bootstrap.sh
+++ bin/java_bootstrap.sh
@@ -7,7 +7,11 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 
 # Add this to accept the Oracle licensing terms without being confronted with a GUI during installation.
-echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 
 # Install the JDK and set all the normal defaults
-sudo apt-get install -y oracle-java7-installer oracle-java7-set-default
+sudo apt-get install -y oracle-java8-installer oracle-java8-set-default
+
+sudo rm -rf /var/lib/apt/lists/*
+sudo rm -rf /var/cache/oracle-jdk8-installer
+
